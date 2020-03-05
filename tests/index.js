5910517e4e8961f8bf24c56f2d98ff327b3411b7
debauchery1st <trevor.j.martin@gmail.com>
2020-03-05 14:44:10 -0500

    volunteer router tests

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,12 @@
 const SuiteAuth = require("./auth-router-test");
 const SuiteBusiness = require("./business-router-test");
+const SuiteVolunteer = require("./volunteer-router-test");
 
 const TestReplateAPI = () =>
   describe("Replate test suites", () => {
     SuiteAuth(); // run first to setup Authorizion token for other tests.
     SuiteBusiness();
+    SuiteVolunteer();
   });
 
 module.exports = TestReplateAPI;
