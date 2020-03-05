70ab9de2d704e9c13f700f8a43fa20657e7603f3
debauchery1st <trevor.j.martin@gmail.com>
2020-03-05 15:41:33 -0500

    test pickupRequests, users

--- tests/index.js
+++ tests/index.js
@@ -1,12 +1,16 @@
 const SuiteAuth = require("./auth-router-test");
+const SuiteUsers = require("./users-router-test");
 const SuiteBusiness = require("./business-router-test");
 const SuiteVolunteer = require("./volunteer-router-test");
+const SuitePickupRequests = require("./pickupRequests-router-test");
 
 const TestReplateAPI = () =>
   describe("Replate test suites", () => {
     SuiteAuth(); // run first to setup Authorizion token for other tests.
+    SuiteUsers();
     SuiteBusiness();
     SuiteVolunteer();
+    SuitePickupRequests();
   });
 
 module.exports = TestReplateAPI;
