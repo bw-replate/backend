c93d06dca223ccee550ab369b3182de3cf587e51
debauchery1st <trevor.j.martin@gmail.com>
2020-03-06 01:35:07 -0500

    apidoc

--- public/api_project.js
+++ public/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-03-06T06:28:24.173Z",
+    "time": "2020-03-06T06:34:56.506Z",
     "url": "http://apidocjs.com",
     "version": "0.20.0"
   }
